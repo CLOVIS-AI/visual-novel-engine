618efe051ee64cc6660385a5f7ea8230b08c9ca7
CLOVIS-AI <clovis.onplag@gmail.com>
2018-02-02 21:11:28 +0100

    Refactored the FileUtility class

--- vne/src/utils/FileUtility.java
+++ vne/src/utils/FileUtility.java
@@ -21,20 +21,58 @@ public class FileUtility {
     public static final FileFilter directories = f -> f.isDirectory();
     
     /**
-     * Checks wether a file is a directory. If it's not, an {@link IllegalArgumentException} is thrown.
+     * Checks whether a file is a directory. If it's not, an {@link IllegalArgumentException} is thrown.
      * @param file the file to be checked.
      * @throws NullPointerException if the file is not a directory.
      */
     public static void assertDirectory(File file){
-        if(file == null)
-            throw new IllegalArgumentException("The file should not be 'null'.");
-        
-        if(!file.exists())
-            throw new NullPointerException("The directory doesn't exist : " + file.getAbsolutePath());
+        assertNotNull(file);
+        assertExists(file);
         
         if(!file.isDirectory())
             throw new NullPointerException("The file is not a directory : " + file.getAbsolutePath());
         
+        assertReadable(file);
+    }
+    
+    /**
+     * Checks whether a file is not a directory. If it's not, an {@link IllegalArgumentException} is thrown.
+     * @param file the file to be checked.
+     * @throws NullPointerException if the file is a directory.
+     */
+    public static void assertFile(File file){
+        assertNotNull(file);
+        assertExists(file);
+        
+        if(!file.isFile())
+            throw new NullPointerException("The file is a directory : " + file.getAbsolutePath());
+        
+        assertReadable(file);
+    }
+    
+    /**
+     * Checks whether a file is null.
+     * @param file the file to be checked.
+     */
+    private static void assertNotNull(File file){
+        if(file == null)
+            throw new IllegalArgumentException("The file should not be 'null'.");
+    }
+    
+    /**
+     * Checks whether a file or directory exists.
+     * @param file the file to be checked.
+     */
+    private static void assertExists(File file){
+        if(!file.exists())
+            throw new NullPointerException("The directory doesn't exist : " + file.getAbsolutePath());
+    }
+    
+    /**
+     * Checks whether a file or directory is readable.
+     * @param file the file to be checked.
+     */
+    private static void assertReadable(File file){
         if(!file.canRead())
             throw new NullPointerException("The directory cannot be read : " + file.getAbsolutePath());
     }
