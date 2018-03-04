da567c413fa45712d5a5930f2d887250d04b24f1
Ivan Canet <ivan.canet@gmail.com>
2018-03-04 20:17:53 +0100

    Split Ressource and TextRessource (allows Images, Sounds...)

--- vne/src/utils/ressources/Ressource.java
+++ vne/src/utils/ressources/Ressource.java
@@ -5,13 +5,11 @@
  */
 package utils.ressources;
 
-import java.io.IOException;
 import java.util.Set;
 import objects.Load;
-import utils.UnloadedException;
 
 /**
- * This interface represents a text-ressource (for instance a file on the
+ * This interface represents a ressource (for instance a file on the
  * machine, or a remote file used with HTTPS, etc).
  * <p>This interface exists as a realization of the Bridge design pattern ; this
  * allows the program to work with any type of ressources instead of using
@@ -19,66 +17,6 @@ import utils.UnloadedException;
  * @author CLOVIS
  */
 public interface Ressource extends Load {
-    
-    /**
-     * Opens this ressource, so that {@link #readByte() } and {@link #readLine() }
-     * might be used.
-     */
-    public void open();
-    
-    /**
-     * Loads this object. Implementations of {@link Ressource} should override 
-     * {@link #open() } instead of this method.
-     */
-    @Override
-    public default void load(){
-        open();
-    }
-    
-    /**
-     * Reads a byte from the ressource and returns it.
-     * <p>This method should never return <code>null</code>, unless no new byte 
-     * are available (ie. {@link #hasNext() hasNext()} returns <code>false</code>).
-     * @return The next byte.
-     * @throws UnloadedException if the ressource has not been openned.
-     * @throws java.io.IOException if any IO exception occurs.
-     * @see #open() Open this ressource (before reading)
-     * @see #close() Close this ressource
-     */
-    public byte readByte() throws UnloadedException, IOException;
-    
-    /**
-     * Reads a line from the ressource and returns it.
-     * <p>This method should never return <code>null</code>, unless no new line 
-     * are available (ie. {@link #hasNext() hasNext()} returns <code>false</code>).
-     * @return The next line.
-     * @throws UnloadedException if the ressource has not been openned.
-     * @throws java.io.IOException if any IO exception occurs.
-     * @see #open() Open this ressource (before reading)
-     * @see #close() Close this ressource
-     */
-    public String readLine() throws UnloadedException, IOException;
-    
-    /**
-     * Does this ressource contain any more bytes, that could be read with
-     * {@link #readByte() }?
-     * @return <code>true</code> if there are bytes remaining.
-     */
-    public boolean hasNext();
-    
-    /**
-     * Closes this ressource, it cannot be read again.
-     * This ressource might be reopened to give access to its content again.
-     */
-    public void close();
-    
-    /**
-     * Is this ressource loaded?
-     * @return <code>true</code> if this ressource is loaded.
-     * @see #open() Load this ressource
-     * @see #close() Unload this ressource
-     */
-    public boolean isLoaded();
     
     /**
      * Gets every children of this ressource (not recursively).
