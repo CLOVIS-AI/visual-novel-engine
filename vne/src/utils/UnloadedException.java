0711e64aacece2dd87cd375b6444391f768d6591
Ivan Canet <ivan.canet@gmail.com>
2018-03-04 19:14:48 +0100

    Added the UnloadedException
    The UnloadedException is used when the program tries to reach a ressource
    that has not been loaded.

---
+++ vne/src/utils/UnloadedException.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+/**
+ * Use this exception when unloaded ressources are accessed.
+ * @author CLOVIS
+ */
+public class UnloadedException extends IllegalStateException {
+
+    /**
+     * Creates a new instance of <code>UnloadedException</code> without detail
+     * message.
+     */
+    public UnloadedException() {
+    }
+
+    /**
+     * Constructs an instance of <code>UnloadedException</code> with the
+     * specified detail message.
+     *
+     * @param msg the detail message.
+     */
+    public UnloadedException(String msg) {
+        super(msg);
+    }
+}
